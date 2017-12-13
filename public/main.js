385ad39300c0fa29de7aa8359a4d036187a406bc
benjaminr177 <unoceptik@gmail.com>
2017-12-13 14:40:10 +0200

    fixed error messaging

--- public/main.js
+++ public/main.js
@@ -24,31 +24,73 @@ function sendData(data) {
   })
 } 
 
-function errorMessage() {
-  error.style.display = 'block';
+var errorMessage = function (msg) {
+  error.style.display = 'block'
+  error.innerText = msg
+}
+
+var checkInput = function (input) {
+  if (input == '' || input == ' ') {
+    errorMessage('URL cannot be empty')
+    return false
+  } else if (!input.includes('http') || !input.includes('https')) {
+    errorMessage('URL is not valid, requires "http://" or "https://"')
+    return false
+  } else {
+    error.style.display = 'none';
+    return true
+  }
 }
 
 // these two event listeners basically do the same thing - form submitting / resetting
 button.addEventListener('click', (e) => {
   e.preventDefault()
 
-  if (input.value != '' || input.value != '') {
+  if (checkInput(input.value)) {
     sendData(input.value)
-    error.style.display = 'none';
-  } else {
-    errorMessage()
   }
 })
 
+// Using keyup instead of change
+// because change will trigger on window resize
 input.addEventListener('keyup', (e) => {
   e.preventDefault()
+
   if (e.keyCode === 13) {
-    if (input.value != '' || input.value != '') {
+    if (checkInput(input.value)) {
       sendData(input.value)
-      error.style.display = 'none';
-    } else {
-      errorMessage()
     }
   }
 })
+
+
+
+
+// function errorMessage() {
+//   error.style.display = 'block';
+// }
+
+// // these two event listeners basically do the same thing - form submitting / resetting
+// button.addEventListener('click', (e) => {
+//   e.preventDefault()
+
+//   if (input.value != '' || input.value != '') {
+//     sendData(input.value)
+//     error.style.display = 'none';
+//   } else {
+//     errorMessage()
+//   }
+// })
+
+// input.addEventListener('keyup', (e) => {
+//   e.preventDefault()
+//   if (e.keyCode === 13) {
+//     if (input.value != '' || input.value != '') {
+//       sendData(input.value)
+//       error.style.display = 'none';
+//     } else {
+//       errorMessage()
+//     }
+//   }
+// })
 
